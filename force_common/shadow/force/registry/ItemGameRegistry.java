0f1f0b4b7cc2b715073e53f282418c279d17ef17
SneakingShadow <mariusdyngen@gmail.com>
2014-05-31 23:06:05 +0200

    Cleanup again

---
+++ force_common/shadow/force/registry/ItemGameRegistry.java
@@ -0,0 +1,12 @@
+package shadow.force.registry.language;
+
+import shadow.force.Force;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class ItemGameRegistry {
+	public ItemGameRegistry(){
+		
+		GameRegistry.registerItem(Force.forceCrystal, "forceCrystal");
+		
+	}
+}
